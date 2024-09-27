d4075593c5c4152060784a7b14a9ced012f1b69e
Oliviaa7 <olivialanderson1994@gmail.com>
2024-09-27 14:29:48 -0700

    added head function

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(val1, val2) {
+  const first = val1[0];
+  return first;
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([2]), 2)
+assertEqual(head([]), 3)
