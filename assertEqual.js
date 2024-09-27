151e190fcf21a38974150c530efbfb5b98a72db5
Oliviaa7 <olivialanderson1994@gmail.com>
2024-09-27 10:21:21 -0700

    adjusted console.logs and format of messages returned

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,12 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return "✅✅✅ Assertion Passed: " + actual + " === " + expected;
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    return "❌❌❌ Assertion Failed: " + actual + " !== " + expected;
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-console.log(assertEqual("We are equal", "We are equal"));
-console.log(assertEqual("Are we equal?", "No we are not"));
-console.log(assertEqual(4, 4));
-console.log(assertEqual(2, 6));
+assertEqual("We are equal", "We are equal");
+assertEqual("Are we equal?", "No we are not");
+assertEqual(4, 4);
+assertEqual(2, 6);
