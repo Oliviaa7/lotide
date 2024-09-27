60fca8734eb5279a0e73513fb0c0c3316cd7ea95
Oliviaa7 <olivialanderson1994@gmail.com>
2024-09-27 15:12:13 -0700

    added ; in head, created tail function

--- head.js
+++ head.js
@@ -13,5 +13,5 @@ const head = function(val1) {
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([2]), 2)
-assertEqual(head([]), undefined)
+assertEqual(head([2]), 2);
+assertEqual(head([]), undefined);
---
+++ tail.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr1) {
+  const end = arr1.slice(1);
+  return end;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const words = ["Yo yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
