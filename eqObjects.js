732abeeea5e0711bd0163c9f47e355327e27c7ce
Oliviaa7 <olivialanderson1994@gmail.com>
2024-10-05 14:04:07 -0700

    adjusted use of eqArrays function within eqObjects per feedback

--- eqObjects.js
+++ eqObjects.js
@@ -31,7 +31,7 @@ const eqObjects = (obj1, obj2) => {
     if (!obj2.hasOwnProperty(key)) {
       return false;
     } else if ((Array.isArray(obj1[key])) && (Array.isArray(obj2[key]))) {
-      if (eqArrays(obj1[key], obj2[key]) === false) {
+      if (!eqArrays(obj1[key], obj2[key])) {
         return false;
       }
     } else if (obj1[key] !== obj2[key]) {
