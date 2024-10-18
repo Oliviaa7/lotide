4939d180b45da048f6d1ff70e1865acc5e3e44b6
Oliviaa7 <olivialanderson1994@gmail.com>
2024-10-18 16:33:02 -0700

    refactored letterPositions with chai/mocha

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,6 +1,14 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const letterPositions = require("../letterPositions");
 
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("Hi my name is Olivia").o, [14]);
-assertArraysEqual(letterPositions("Hi my name is Olivia").i, [1, 11, 16, 18]);
+describe('#letterPositions', () => {
+  it("returns [1] for the position of letter e in 'hello'", () => {
+    assert.deepEqual(letterPositions("hello").e, [1]);
+  });
+  it("returns [14] for the position of letter o in 'Hi my name is Olivia'", () => {
+    assert.deepEqual(letterPositions("Hi my name is Olivia").o, [14]);
+  });
+  it("returns [1, 11, 16, 18] for the positions of letter i in 'Hi my name is Olivia'", () => {
+    assert.deepEqual(letterPositions("Hi my name is Olivia").i, [1, 11, 16, 18]);
+  })
+});
