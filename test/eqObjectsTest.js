db947239edd38ed3b9c0ed592fb6d3e8aa2eee8d
Oliviaa7 <olivialanderson1994@gmail.com>
2024-10-18 16:49:19 -0700

    refactored eqObjects with mocha/chai

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,21 +1,29 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const eqObjects = require("../eqObjects");
 
-const shirtObject = { colour: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", colour: "red" };
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
-
-const longSleeveShirtObject = { size: "medium", colour: "red", sleeveLength: "long"};
-assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
-
-const multiColourShirtObject = { colours: ["red", "blue"], size: "medium" };
-const anotherMultiColourShirtObject = { size: "medium", colours: ["red", "blue"]};
-assertEqual(eqObjects(multiColourShirtObject, anotherMultiColourShirtObject), true);
-
-const longSleeveMultiColourShirtObject = {
-  size: "medium",
-  colours: ["red", "blue"],
-  sleeveLength: "long",
-};
-
-assertEqual(eqObjects(multiColourShirtObject, longSleeveMultiColourShirtObject), false);
+describe('#eqObjects', () => {
+  it("returns true when two objects have the same key/value pairs", () => {
+    const shirtObject = { colour: "red", size: "medium" };
+    const anotherShirtObject = { size: "medium", colour: "red" };
+    assert.deepEqual(eqObjects(shirtObject, anotherShirtObject), true);
+  });
+  it("returns false when two objects do not have the same key/value pairs", () => {
+    const shirtObject = { colour: "red", size: "medium" };
+    const longSleeveShirtObject = { size: "medium", colour: "red", sleeveLength: "long"};
+    assert.deepEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+  });
+  it("returns true when two objects whose keys contain arrays have the same key/value pairs", () => {
+    const multiColourShirtObject = { colours: ["red", "blue"], size: "medium" };
+    const anotherMultiColourShirtObject = { size: "medium", colours: ["red", "blue"]};
+    assert.deepEqual(eqObjects(multiColourShirtObject, anotherMultiColourShirtObject), true);
+  });
+  it("returns false when two objects whose keys contain arrays do not have the same key/value pairs", () => {
+    const multiColourShirtObject = { colours: ["red", "blue"], size: "medium" };
+    const longSleeveMultiColourShirtObject = {
+        size: "medium",
+        colours: ["red", "blue"],
+        sleeveLength: "long",
+      };
+    assert.deepEqual(eqObjects(multiColourShirtObject, longSleeveMultiColourShirtObject), false)
+  });
+});
