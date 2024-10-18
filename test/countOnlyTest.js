18029c1c93db3cf33ba1b44072975fe774cd31b5
Oliviaa7 <olivialanderson1994@gmail.com>
2024-10-18 16:25:41 -0700

    refactored countOnly with mocha/chai

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -1,26 +1,47 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const countOnly = require("../countOnly");
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, {
-  Jason: true,
-  Karima: true,
-  Fang: true,
-  Agouhanna: false,
+describe('#countOnly', () => {
+  it("returns {Fang: 2} for a list of names which contains Fang twice", () => {
+    const firstNames = [
+        "Karl",
+        "Salima",
+        "Agouhanna",
+        "Fang",
+        "Kavith",
+        "Jason",
+        "Salima",
+        "Fang",
+        "Joe"
+      ];
+      assert.deepEqual(countOnly(firstNames, {Fang: true}), {Fang: 2});
+  });
+  it("returns an empty object for a list of names which does not contain Karima", () => {
+    const firstNames = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+    assert.deepEqual(countOnly(firstNames, {Karima: true}), {});
+  })
+  it("returns an empty object for a list of names which does contain Agouhanna but for which we have not requested that information", () => {
+    const firstNames = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+    assert.deepEqual(countOnly(firstNames, {Agouhanna: false}), {});
+  })
 });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
