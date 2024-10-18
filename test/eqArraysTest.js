f77fa3cc2a73697897394d8d601b4f349b0d8b7c
Oliviaa7 <olivialanderson1994@gmail.com>
2024-10-18 16:56:20 -0700

    refactored eqArrays with mocha/chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,23 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const eqArrays = require("../eqArrays");
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([3, 4, 5], [3, 4, "5"]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([], [1]), false);
+describe('#eqArrays', () => {
+  it("should return true if two arrays are equal", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("should return false for the following two arrays: [3, 4, 5], [3, 4, '5']", () => {
+    assert.deepEqual(eqArrays([3, 4, 5], [3, 4, "5"]), false);
+  });
+  it("should return true for the following two arrays: ['1', '2', '3'], ['1', '2', '3']", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+  it("should return false for the following two arrays: [1, 2, 3], [1, 2]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2]), false);
+  });
+  it("should return true for two empty arrays", () => {
+    assert.deepEqual(eqArrays([], []), true);
+  });
+  it("should return false for one empty array and one array containing values", () => {
+    assert.deepEqual(eqArrays([], [1]), false);
+  });
+});
