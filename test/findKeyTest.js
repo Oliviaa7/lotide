b4e804231fb26b543ab40393254f5572d37ad170
Oliviaa7 <olivialanderson1994@gmail.com>
2024-10-18 17:10:59 -0700

    refactored findKeyTest with mocha/chai

--- test/findKeyTest.js
+++ test/findKeyTest.js
@@ -1,13 +1,17 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const findKey = require("../findKey");
 
-assertEqual(findKey(
-  {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 },
-  },
-  (x) => x.stars === 2), "noma");
+describe('#findKey', () => {
+  it("should use a callback, 'x.stars === 2' to determine a key, 'noma' in an object of stars", () => {
+    assert.deepEqual(findKey(
+      {
+        "Blue Hill": { stars: 1 },
+        Akaleri: { stars: 3 },
+        noma: { stars: 2 },
+        elBulli: { stars: 3 },
+        Ora: { stars: 2 },
+        Akelarre: { stars: 3 },
+      },
+      (x) => x.stars === 2), "noma")
+  });
+});
