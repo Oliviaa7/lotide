1810a55bc1b228503ca3f5ae48b600d9013fd8c0
Oliviaa7 <olivialanderson1994@gmail.com>
2024-10-18 17:37:28 -0700

    refactored withoutTest with mocha/chai

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -1,13 +1,20 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const without = require("../without");
 
-assertArraysEqual((without([1, 2, 3], [1])), [2, 3]);
-assertArraysEqual((without(["1", "2", "3"], [1, 2, "3"])), ["1", "2"]);
-assertArraysEqual((without(["freddy", "leo", "zoe"], ["zoe"])), ["freddy", "leo"]);
-assertArraysEqual((without([5, 6, 7], [1, 2, 3])), [5, 6, 7]);
-assertArraysEqual((without([], ["empty", "huh"])), []);
-assertArraysEqual((without(["stuff", "here"], [])), ["stuff", "here"]);
-
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]);
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+describe('#without', () => {
+  it("takes [1, 2, 3] and [1] and returns [2, 3]", () => {
+    assert.deepEqual((without([1, 2, 3], [1])), [2, 3]);
+  });
+  it("takes ['1', '2', '3'] and [1, 2, '3'] and returns ['1', '2']", () => {
+    assert.deepEqual((without(["1", "2", "3"], [1, 2, "3"])), ["1", "2"]);
+  });
+  it("takes [5, 6, 7] and [1, 2, 3] and returns [5, 6, 7]", () => {
+    assert.deepEqual((without([5, 6, 7], [1, 2, 3])), [5, 6, 7]);
+  });
+  it("takes [] and ['empty, 'huh'] and returns []", () => {
+    assert.deepEqual((without([], ["empty", "huh"])), []);
+  });
+  it("takes ['stuff', 'here'] and [] and returns ['stuff', 'here']", () => {
+    assert.deepEqual((without(["stuff", "here"], [])), ["stuff", "here"]);
+  });
+});
