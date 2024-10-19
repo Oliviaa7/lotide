e9682deec64aa40f91a198d7902ce0c7bbaa6f1c
Oliviaa7 <olivialanderson1994@gmail.com>
2024-10-18 17:06:45 -0700

    refactored countLetters with mocha/chai

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,8 +1,11 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const countLetters = require("../countLetters");
 
-const result2 = countLetters("lighthouse in the house");
-const result3 = countLetters("Hi my name is Olivia");
-
-assertEqual(result2['t'], 2);
-assertEqual(result3['i'], 4);
+describe('#countLetters', () => {
+  it("returns 2 for the number of 't's in the string 'lighthouse in the house'", () => {
+    assert.strictEqual(countLetters("lighthouse in the house").t, 2);
+  });
+  it("returns 4 for the number of 'i's in the string 'Hi my name is Olivia'", () => {
+    assert.strictEqual(countLetters("Hi my name is Olivia").i, 4);
+  })
+});
