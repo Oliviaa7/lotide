99c18a0ac5962d3eb0f9e8fca8cb5e30db70fff4
Oliviaa7 <olivialanderson1994@gmail.com>
2024-10-18 17:24:21 -0700

    refactored mapTest with mocha/chai

--- test/mapTest.js
+++ test/mapTest.js
@@ -1,11 +1,21 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const map = require("../map");
 
-const words = ["you", "look", "like", "stevie", "nicks"];
-const nums = [1, 4, 2, 7, 9, 10];
-const emptyArray = [];
-
-assertArraysEqual(map(words, (word) => word[2]), ["u", "o", "k", "e", "c"]);
-assertArraysEqual(map(nums, (num) => num + 3), [4, 7, 5, 10, 12, 13]);
-assertArraysEqual(map(words, (word) => `${word} 👀`), ["you 👀", "look 👀", "like 👀", "stevie 👀", "nicks 👀"]);
-assertArraysEqual(map(emptyArray, 50), "Invalid Input!");
+describe('#map', () => {
+  it("takes an array, ['you', 'look', 'like', 'stevie', 'nicks'], and a callback, (word) => word[2]), and returns a new array, ['u', 'o', 'k', 'e', 'c'", () => {
+    const words = ["you", "look", "like", "stevie", "nicks"];
+    assert.deepEqual(map(words, (word) => word[2]), ["u", "o", "k", "e", "c"])
+  });
+  it("returns [4, 7, 5, 10, 12, 13] when passed [1, 4, 2, 7, 9, 10] and (num) => num + 3)", () => {
+    const nums = [1, 4, 2, 7, 9, 10];
+    assert.deepEqual(map(nums, (num) => num + 3), [4, 7, 5, 10, 12, 13]);
+  });
+  it("returns ['you 👀', 'look 👀', 'like 👀', 'stevie 👀', 'nicks 👀'] when passed ['you', 'look', 'like', 'stevie', 'nicks'] and (word) => `${word} 👀`)", () => {
+    const words = ["you", "look", "like", "stevie", "nicks"];
+    assert.deepEqual(map(words, (word) => `${word} 👀`), ["you 👀", "look 👀", "like 👀", "stevie 👀", "nicks 👀"]);
+  });
+  it("returns 'Invalid Input!' when passed an empty array and/or not a callback function", () => {
+    const emptyArray = [];
+    assert.deepEqual(map(emptyArray, 50), "Invalid Input!");
+  });
+});
